39046050ce45ea0b225748a72a7443c52689977e
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-05-24 19:17:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import turtle
 
-# Configurar a tela
 screen = turtle.Screen()
 screen.setup(600, 600)
 screen.bgcolor("pink")
 
-# Criar a tartaruga
 pen = turtle.Turtle()
 pen.shape("turtle")
 pen.speed(2)
@@ -13,7 +11,6 @@ pen.color("red")
 pen.fillcolor("red")
 pen.penup()
 
-# Desenhar o coração
 pen.goto(0, -50)
 pen.pendown()
 pen.begin_fill()
@@ -25,15 +22,12 @@ pen.circle(-90, 200)
 pen.forward(180)
 pen.end_fill()
 
-# Esconder a tartaruga temporariamente
 pen.penup()
 pen.goto(0, -150)
 pen.color("black")
-pen.write("I love you", align="center", font=("Arial", 18, "bold"))
+pen.write("Você não é um eletrocardiograma, mas fez meu coração dar uma arritmada de tanto amor", align="center", font=("Arial", 18, "bold"))
 pen.hideturtle()
 
-# Mostrar a tartaruga novamente
 pen.showturtle()
 
-# Manter a janela aberta
 turtle.done()
